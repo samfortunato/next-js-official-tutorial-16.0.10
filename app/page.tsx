9bc2a6cf5d12b282cc090e2a77fe55b412881119
Samuel Fortunato <samfortunato@gmail.com>
2026-04-01 16:03:42 -0400

    Add hero image for larger screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import AcmeLogo from '@/app/ui/acme-logo';
 
@@ -37,6 +38,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
