cbb553a29f4e7da617776f7d06d0c426a74f6acc
Samuel Fortunato <samfortunato@gmail.com>
2026-04-01 15:51:06 -0400

    Add Vercel CSS module logo (currently unused)

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import styles from '@/app/ui/home.module.css';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -13,6 +15,8 @@ export default function Page() {
           <div
             className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
+
+          {/* <div className={styles.shape} /> */}
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
