c4b3e8c4801bf14b9ca1d37a494e622b646024a1
Samuel Fortunato <samfortunato@gmail.com>
2026-04-04 21:25:36 -0400

    Add ability to search to filter invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,7 +8,16 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
 import { lusitana } from '@/app/ui/fonts';
 
-export default async function Page() {
+export default async function Page(props: {
+	searchParams?: Promise<{
+		query?: string,
+		page?: string,
+	}>
+}) {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+
 	return (
 		<div className="w-full">
 			<div className="flex w-full items-center justify-between">
@@ -22,9 +31,9 @@ export default async function Page() {
 				<CreateInvoice />
 			</div>
 
-			{/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+			<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
 				<Table query={query} currentPage={currentPage} />
-			</Suspense> */}
+			</Suspense>
 
 			<div className="mt-5 flex w-full justify-center">
 				{/* <Pagination totalPages={totalPages} /> */}
